881e07e4952761602cf0e5967d16bbac67c3005b
Mehmet Emin TOK <95161664+mehmeteminnnn@users.noreply.github.com>
2022-08-25 12:36:19 +0300

    Formlar arasında geciş yapma

---
+++ FormlarArasıGeçiş/FormlarArasıGeçiş/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormlarArasıGeçiş
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text=="admin"&& textBox2.Text=="1234")
+            {
+                Form2 form = new Form2();
+                form.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Hatalı Giriş!!!");
+            }
+        }
+    }
+}
---
+++ FormlarArasıGeçiş/FormlarArasıGeçiş/Form2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormlarArasıGeçiş
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
